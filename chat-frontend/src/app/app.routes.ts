7f64982a2d154726c15a5dbdf8d063c2a7f74d20
nextlevel7 <suzon9849@gmail.com>
2026-09-20 16:45:22 +0545

    [intial]: frontend setup

---
+++ chat-frontend/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+   {
+    path: 'login',
+    loadComponent: () => 
+        import("./features/auth/pages/login/login").then((m) => m.Login)
+   },
+   {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+   },
+];
